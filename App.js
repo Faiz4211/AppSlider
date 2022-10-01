543b78299f05793bda2943e097078292ac083fbe
Faiz4211 <faizk2631@gmail.com>
2022-10-01 11:33:08 +0500

    App Slider code

---
+++ App.js
@@ -0,0 +1,96 @@
+import React from "react";
+import { Image, StyleSheet, Text, View } from "react-native";
+import AppIntroSlider from "react-native-app-intro-slider";
+// import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+
+const slides = [
+  {
+    key: "one",
+    title: "JUST TRAVEL",
+    text:
+      "Lorem ipsum dolor sit amet consecte tuer adipsing elit sed diam monum my nibh eusimod eltor",
+    image: require("./src/images/1.png"),
+  },
+  {
+    key: "two",
+    title: "TAKE A BREAK",
+    text:
+      "Lorem ipsum dolor sit amet consecte tuer adipsing elit sed diam monum my nibh eusimod eltor",
+    image: require("./src/images/2.png"),
+  },
+  {
+    key: "three",
+    title: "ENJOY YOUR JOURNEY",
+    text:
+      "Lorem ipsum dolor sit amet consecte tuer adipsing elit sed diam monum my nibh eusimod eltor",
+    image: require("./src/images/3.png"),
+  },
+];
+
+export default class App extends React.Component {
+  state = { showHomePage: false };
+  _renderItem = ({ item }) => {
+    return (
+      <View style={{ flex: 1 }}>
+        <Image
+          source={item.image}
+          style={{
+            resizeMode: "cover",
+            height: "73%",
+            width: "100%",
+          }}
+        />
+        <Text
+          style={{
+            paddingTop: 25,
+            paddingBottom: 10,
+            fontSize: 23,
+            fontWeight: "bold",
+            color: "#21465b",
+            alignSelf: "center",
+          }}
+        >
+          {item.title}
+        </Text>
+
+        <Text style={{
+          textAlign: "center",
+          color: "#21465b",
+          fontWeight: '400',
+          fontSize: 15,
+          paddingHorizontal: 30
+        }}>
+          {item.text}
+        </Text>
+      </View>
+    );
+  };
+
+  render() {
+    if (this.state.showHomePage) {
+      return <App />
+    } else
+      return (
+        <AppIntroSlider
+          renderItem={this._renderItem}
+          data={slides}
+          showNextButton={false}
+          showDoneButton={false}
+          activeDotStyle={{
+            backgroundColor: "#21465b",
+            width: 30
+          }}
+        />
+
+      );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
